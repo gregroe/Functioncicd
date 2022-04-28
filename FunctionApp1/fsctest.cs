7a28a870f4d125d155184022ccd768d81e0311fa
Greg Roe <gregroe@msn.com>
2022-04-28 06:49:37 -0700

    testing github connectivity

--- FunctionApp1/fsctest.cs
+++ FunctionApp1/fsctest.cs
@@ -21,7 +21,7 @@ namespace FunctionApp1
             [Queue("outqueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Greg test C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
